ee4c6a1aa9b085d7a9ba63ab9e13b24507d40308
M <M>
2021-04-21 15:10:49 +0600

    upload all

---
+++ js/tinymce/tester/tester/tester/test.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--<script src="./jquery.tinymce.min.js" ></script>-->
+    <script src="../../../tinymce.min.js" ></script>
+
+    <script>
+/*       tinymce.init({
+        selector: '#mytextarea',
+		height : 300,
+		 plugins: 'image',
+		menubar: 'insert',
+		toolbar: 'image',
+		image_list: [
+			{title: 'My image 1', value: 'https://www.example.com/my1.gif'},
+			{title: 'My image 2', value: 'http://www.moxiecode.com/my2.gif'}
+		]
+      }); */
+	  var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+tinymce.init({
+  selector: 'textarea#mytextarea',
+  plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+  
+  /* plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars responsivefilemanager emoticons', */
+  imagetools_cors_hosts: ['picsum.photos'],
+  /* enable title field in the Image dialog*/
+  image_title: true,
+  /* enable automatic uploads of images represented by blob or data URIs*/
+  automatic_uploads: true,
+  images_upload_url: '../../../image_upload.php',
+  menubar: 'file edit view insert format tools table help',
+  toolbar1: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons |',
+  toolbar2: 'fullscreen  preview save print insertfile image media template link anchor codesample | ltr rtl',
+  /* toolbar2: 'fullscreen  preview save print | responsivefilemanager |insertfile image media template link anchor codesample | ltr rtl', */
+  toolbar_sticky: false,
+  autosave_ask_before_unload: true,
+  autosave_interval: '30s',
+  autosave_prefix: '{path}{query}-{id}-',
+  autosave_restore_when_empty: false,
+  autosave_retention: '2m',
+  image_advtab: true,
+  importcss_append: true,
+  /* and here's our custom image picker*/
+  file_picker_callback: function (callback, value, meta) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    /*
+      Note: In modern browsers input[type="file"] is functional without
+      even adding it to the DOM, but that might not be the case in some older
+      or quirky browsers like IE, so you might want to add it to the DOM
+      just in case, and visually hide it. And do not forget do remove it
+      once you do not need it anymore.
+    */
+
+    input.onchange = function () {
+      var file = this.files[0];
+
+      var reader = new FileReader();
+      reader.onload = function () {
+        /*
+          Note: Now we need to register the blob in TinyMCEs image blob
+          registry. In the next release this part hopefully won't be
+          necessary, as we are looking to handle it internally.
+        */
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        var base64 = reader.result.split(',')[1];
+        var blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        /* call the callback and populate the Title field with the file name */
+        callback(blobInfo.blobUri(), { title: file.name });
+      };
+      reader.readAsDataURL(file);
+    };
+
+    input.click();
+  },
+  templates: [
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+  ],
+  template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+  template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+  height: 300,
+  image_caption: true,
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quicktable',
+  noneditable_noneditable_class: 'mceNonEditable',
+  toolbar_mode: 'sliding',
+  contextmenu: 'link image imagetools table',
+  skin: useDarkMode ? 'oxide-dark' : 'oxide',
+  content_css: useDarkMode ? 'dark' : 'default',
+/*    external_filemanager_path:"filemanager/",
+   filemanager_title:"Responsive Filemanager" ,
+   external_plugins: { "responsivefilemanager" : "./plugins/responsivefilemanager/plugin.min.js","filemanager": "./filemanager/plugin.min.js"}, */
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+ });
+
+    </script>
+
+  </head>
+<?php
+if(!empty($_POST['value'])){echo($_POST['value']);}
+?>
+
+
+  <body>
+  <h1>TinyMCE Quick Start Guide</h1>
+    <form method="post">
+      <textarea name="value" id="mytextarea">Hello, World!</textarea>
+	  <input type="submit"/>
+    </form>
+  </body>
+</html>
